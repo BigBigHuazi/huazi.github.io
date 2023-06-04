43ca3fbe97acc904cfd5e60893097053112459ac
BigBigHuazi <bighuazi@qq.com>
2023-06-05 01:10:53 +0800

    Site updated: 2023-06-05 01:10:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/02/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/06/02/hello-world/","2023/05/24/Vol-070/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
